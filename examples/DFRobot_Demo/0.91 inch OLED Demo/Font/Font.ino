aad90eb07454fdfb384a817c367681920b403469
Frances9 <44355133+Frances9@users.noreply.github.com>
2021-12-24 09:43:14 +0800

    Update Font.ino

--- examples/DFRobot_Demo/0.91 inch OLED Demo/Font/Font.ino
+++ examples/DFRobot_Demo/0.91 inch OLED Demo/Font/Font.ino
@@ -1,8 +1,8 @@
 /*!
  * @file Font.ino
- * @brief U8G2中几种支持字体的显示
- * @n U8G2支持多种字体，本demo只是选取几种字体进行显示
- * @n U8G2字体GitHub连接：https://github.com/olikraus/u8g2/wiki/fntlistall
+ * @brief Display of several fonts that U8G2 supported
+ * @n U8G2 supports kinds of fonts, this demo just displays part of them
+ * @n U8G2 font GitHub link: https://github.com/olikraus/u8g2/wiki/fntlistall
  * 
  * @copyright  Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
  * @licence     The MIT License (MIT)
@@ -18,31 +18,31 @@
 #include <Wire.h>
 
 /*
- *IIC构造函数
- *@param 指定大局显示的基本布局：	U8G2_R0 不旋转，横向，绘制方向从左到右
-									U8G2_R1 顺时针旋转90度，绘制方向从上到下
-									U8G2_R2 顺时针旋转180度，绘制方向从右到左
-									U8G2_R3 顺时针旋转270度，绘制方向从下到上
-									U8G2_MIRROR 正常显示镜像内容（v2.6.x版本以上使用)   注意:U8G2_MIRROR需要与setFlipMode（）配搭使用.
- *@param reset：U8x8_PIN_NONE 表示引脚为空，不会使用复位引脚
+ *IIC constructor
+ *@param Determine the overall layout: 	 U8G2_R0 no rotation, landscape, draw from left to right
+									U8G2_R1 Rotate 90 degree clockwise, draw from top to the bottom
+									U8G2_R2 Rotate 180 degree clockwise, draw from right to the left
+									U8G2_R3 Rotate 270 degree clockwise, draw from bottom to the top
+									U8G2_MIRROR Display mirror content normally (used by v2.6.x version or later)   Note: U8G2_MIRROR need using with setFlipMode ()
+ *@param reset：U8x8_PIN_NONE represent empty pin, will not use reset pin
  *
 */
 U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);  //  M0/ESP32/ESP8266/mega2560/Uno/Leonardo
 
 
 void setup(void){
-  u8g2.begin();            //初始化函数
-  u8g2.setFontPosTop();   /*使用drawStr显示字符串时，默认标准为显示字符的左下角坐标。
-                          本函数的功能可理解为将坐标位置改为显示字符串的左上角为坐标标准。*/
+  u8g2.begin();            //Init function
+  u8g2.setFontPosTop();   /*When using drawStr to display a character string, the default is to display the character coordinates at the bottom-left corner.
+                          The function is used to change the coordinate position to the upper-left corner where displays character string.*/
     }
 
 void loop(void){
-  u8g2.clearBuffer();            //清除缓存  
-  u8g2.setFont(u8g2_font_maniac_tr); //设置“u8g2_font_maniac_tr”为字体 
-  u8g2.drawStr(/* x=*/0, /* y=*/5, "DFR123");  //在x=0，y=5的坐标开始绘制字符串“DFR123”  
-  u8g2.sendBuffer();          //发送缓存到显示器      
+  u8g2.clearBuffer();            //Clear buffer  
+  u8g2.setFont(u8g2_font_maniac_tr); //Set "u8g2_font_maniac_tr" as a font 
+  u8g2.drawStr(/* x=*/0, /* y=*/5, "DFR123");  //Start to draw the character string "DFR123" at the coordinates of x=0，y=5  
+  u8g2.sendBuffer();          //Send buffer to the display      
   
-  delay(1000);               //等待1000ms
+  delay(1000);               //Delay 1000ms
    
   u8g2.clearBuffer();             
   
