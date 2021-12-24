ded80a25197128e18bff14a874a4cd6fc14798d8
Frances9 <44355133+Frances9@users.noreply.github.com>
2021-12-24 10:11:30 +0800

    Update FontDirection.ino

--- examples/DFRobot_Demo/0.91 inch OLED Demo/FontDirection/FontDirection.ino
+++ examples/DFRobot_Demo/0.91 inch OLED Demo/FontDirection/FontDirection.ino
@@ -1,6 +1,6 @@
 /*!
  * @file FontDirection.ino
- * @brief U8G2字体的旋转显示
+ * @brief U8G2 font rotation display
  * 
  * @copyright  Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
  * @licence     The MIT License (MIT)
@@ -16,13 +16,13 @@
 #include <Wire.h>
 
 /*
- *IIC构造函数
- *@param 指定大局显示的基本布局：	U8G2_R0 不旋转，横向，绘制方向从左到右
-									U8G2_R1 顺时针旋转90度，绘制方向从上到下
-									U8G2_R2 顺时针旋转180度，绘制方向从右到左
-									U8G2_R3 顺时针旋转270度，绘制方向从下到上
-									U8G2_MIRROR 正常显示镜像内容（v2.6.x版本以上使用)   注意:U8G2_MIRROR需要与setFlipMode（）配搭使用.
- *@param reset：U8x8_PIN_NONE 表示引脚为空，不会使用复位引脚
+ *IIC constructor
+ *@param Determine the overall layout: 	 U8G2_R0 no rotation, landscape, draw from left to right
+									U8G2_R1 Rotate 90 degree clockwise, draw from top to the bottom
+									U8G2_R2 Rotate 180 degree clockwise, draw from right to the left
+									U8G2_R3 Rotate 270 degree clockwise, draw from bottom to the top
+									U8G2_MIRROR Display mirror content normally (used by v2.6.x version or later)   Note: U8G2_MIRROR need using with setFlipMode ()
+ *@param reset：U8x8_PIN_NONE represent empty pin, will not use reset pin
  *
 */
 U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);  //  M0/ESP32/ESP8266/mega2560/Uno/Leonardo
@@ -34,31 +34,31 @@ const uint8_t col[] U8X8_PROGMEM= {0x00,0xc0,0x00,0x00,0x00,0xe0,0x01,0x00,0x00,
                                          0xe0,0xff,0xff,0x01,0x20,0x00,0x00,0x01,0xa0,0xff,0x7f,0x01,0xa0,0x01,0x60,0x01,
                                          0x20,0x07,0x38,0x01,0xe0,0x0c,0xcc,0x01,0xe0,0x39,0xe7,0x01,0xe0,0xe7,0xf9,0x01,
                                          0xc0,0x1f,0xfe,0x00,0x80,0xff,0x7f,0x00,0x00,0xfe,0x1f,0x00,0x00,0xf8,0x07,0x00,
-                                         0x00,0xe0,0x01,0x00,0x00,0xc0,0x00,0x00}; // 图片的数据，尺寸为30x30
+                                         0x00,0xe0,0x01,0x00,0x00,0xc0,0x00,0x00}; // Graphics data, size is 30x30
 
                                          
 
 void setup() {
-  u8g2.begin();  //初始化函数
+  u8g2.begin();  //Init function
 }
 
 void loop(void)
 { 
-    u8g2.clearBuffer();    //清除缓存
+    u8g2.clearBuffer();    //Clear buffer
 
-    u8g2.drawXBMP( /* x=*/0 , /* y=*/0 , /* width=*/30 , /* height=*/30 , col );     //30,30为图片尺寸，根据你的图片尺寸修改
+    u8g2.drawXBMP( /* x=*/0 , /* y=*/0 , /* width=*/30 , /* height=*/30 , col );     //Graphics size is 30x30 and can be changed based on the actual size.
     
-    u8g2.setFont(u8g2_font_pxplusibmvga9_tf);   //选择“u8g2_font_pxplusibmvga9_tf”为字体
+    u8g2.setFont(u8g2_font_pxplusibmvga9_tf);   //Select "u8g2_font_pxplusibmvga9_tf" as a font
     for(int i = 0; i < 4;i++)
     {
       switch(i)
       {
         case 0:
-        /*@brief 设置所有字符串或字形的绘制方向setFontDirection(uint8_t dir)
-         *@param dir=0，旋转0度
-				 dir=1，旋转90度
-				 dir=2，旋转180度
-				 dir=3，旋转270度
+        /*@brief Set the drawing direction of all the character string or fonts setFontDirection(uint8_t dir)
+         *@param dir=0, Rotate 0 degree
+				 dir=1, rotate 90 degree
+				 dir=2, rotate 180 degree
+				 dir=3, rotate 270 degree
         */
         u8g2.setFontDirection(0);           
         u8g2.drawStr(/* x=*/33, /* y=*/32, "DFR");
@@ -80,7 +80,7 @@ void loop(void)
         break;
       }
       delay(500);
-      u8g2.sendBuffer();//发送缓存数据到显示屏
+      u8g2.sendBuffer();//Send buffer data to the display
     }
 
 }
