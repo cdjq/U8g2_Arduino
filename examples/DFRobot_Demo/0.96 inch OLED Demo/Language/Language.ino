4cfad51a23fc4a9952e8a11a0489b45d5ec1ca0b
Frances9 <44355133+Frances9@users.noreply.github.com>
2021-12-27 15:26:41 +0800

    Update Language.ino

--- examples/DFRobot_Demo/0.96 inch OLED Demo/Language/Language.ino
+++ examples/DFRobot_Demo/0.96 inch OLED Demo/Language/Language.ino
@@ -1,8 +1,8 @@
 /*!
  * @file Font.ino
- * @brief U8G2中支持多种语言的显示
- * @n U8G2支持多种语言，此demo展示了中、英、韩、日四国语言。
- * @n U8G2字体GitHub连接：https://github.com/olikraus/u8g2/wiki/fntlistall
+ * @brief U8G2 supports multi-language display
+ * @n U8G2 supports multiple languages, this demo displayed Chinese, English, Korean and Japanese.
+ * @n U8G2 font GitHub link: https://github.com/olikraus/u8g2/wiki/fntlistall
  * 
  * @copyright  Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
  * @licence     The MIT License (MIT)
@@ -22,16 +22,16 @@
 
 
 /*
- *IIC构造函数
- *@param 指定大局显示的基本布局：	U8G2_R0 不旋转，横向，绘制方向从左到右
-									U8G2_R1 顺时针旋转90度，绘制方向从上到下
-									U8G2_R2 顺时针旋转180度，绘制方向从右到左
-									U8G2_R3 顺时针旋转270度，绘制方向从下到上
-									U8G2_MIRROR 正常显示镜像内容（v2.6.x版本以上使用)   注意:U8G2_MIRROR需要与setFlipMode（）配搭使用.
- *@param reset：U8x8_PIN_NONE 表示引脚为空，不会使用复位引脚
- *SPI构造函数
- *@param  cs 按引脚接上即可（引脚可自己选择）
- *@param  dc 按引脚接上即可（引脚可自己选择）
+ *IIC constructor
+ *@param Determine the overall layout:	 U8G2_R0 no rotation, landscape, draw from left to right
+									U8G2_R1 Rotate 90 degree clockwise, draw from top to the bottom
+									U8G2_R2 Rotate 180 degree clockwise, draw from right to the left
+									U8G2_R3 Rotate 270 degree clockwise, draw from bottom to the top
+									U8G2_MIRROR Display mirror content normally (used by v2.6.x version or later)   Note: U8G2_MIRROR need using with setFlipMode ()
+ *@param reset：U8x8_PIN_NONE represent empty pin, will not use reset pin
+ *SPI constructor
+ *@param  cs Connect to the corresponding pin (select pin as per your needs)
+ *@param  dc Connect to the corresponding pin (select pin as per your needs)
  *
 */
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);  //  M0/ESP32/ESP8266/mega2560/Uno/Leonardo
@@ -40,26 +40,26 @@ U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);  /
 
 
 void setup(void) {
-  u8g2.begin();    //初始化函数
-  u8g2.enableUTF8Print();		// 为Arduino print（）函数启用UTF8支持
+  u8g2.begin();    //Init function
+  u8g2.enableUTF8Print();		// Enable UTF8 to support Arduino print () function
 }
 
 void loop(void) 
 {
-  /*字库占用的内存较大，谨慎使用。若只需显示固定几个字，可自行获得汉字编码，
-  通过drawXBM的方法去显示。或者使用内存更大的主控*/
-  u8g2.setFont(u8g2_font_unifont_t_chinese2);  //对“你好世界”的所有字形使用chinese2
-  //u8g2.setFont(u8g2_font_b10_t_japanese1);  // 日语中已经包含了所有“こんにちは世界”的字形1：Lerning 1-6级
-  //u8g2.setFont(u8g2_font_unifont_t_korean1);  // 日语中已经包含了所有“안녕하세요세계”的字形：Lerning 1-2级
-  u8g2.setFontDirection(0);   //设置字体方向，旋转0度,正常方向显示
+  /*Note that the character library occupies large memory. If only a few characters need displaying, the coding of Chinese characters can be obtained,
+  Display them through the method of drawXBM, or use the MCU with larger memory*/
+  u8g2.setFont(u8g2_font_unifont_t_chinese2);  //Apply chinese2 to all the fonts of "hello world"
+  //u8g2.setFont(u8g2_font_b10_t_japanese1);  // All the fonts of "こんにちは世界" are contained in Japanese: Lerning 1-6 level
+  //u8g2.setFont(u8g2_font_unifont_t_korean1);  // All the fonts of "안녕하세요세계" are contained in Korean: Lerning 1-2 level
+  u8g2.setFontDirection(0);   //Set font direction, rotate 0 degree, display in normal direction
   /*
-   * firstPage方法会把当前页码位置变成0
-   * 修改内容处于firstPage和nextPage之间，每次都是重新渲染所有内容
-   * 该方法消耗的ram空间，比sendBuffer消耗的ram空间要少
+   * firstPage method will change the current page number to 0
+   * The content to be modified is between firstPage and nextPage; need to re-render all the content every time
+   * The method consumed less ram than sendBuffer did
   */ 
   u8g2.firstPage();
   do {
-    u8g2.setCursor(/* x=*/0, /* y=*/15);    //定义打印功能的光标,打印 功能的任何输出都将在此位置开始
+    u8g2.setCursor(/* x=*/0, /* y=*/15);    //Define the print function cursor, where any output of the print function starts
     u8g2.print("Hello World!");
     u8g2.setCursor(0, 30);
     u8g2.print("你好世界");		// Chinese "Hello World" 
